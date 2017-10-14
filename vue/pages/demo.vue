5f59bb842a65764c14e2482654e12171e1616ea1
shawn <qiusct@gmail.com>
2017-10-14 23:48:57 +0800

    demo.vue

--- vue/pages/demo.vue
+++ vue/pages/demo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <HeaderBar/>
     <div class="list">hello</div>
     <div class="art">ok</div>
@@ -9,6 +9,25 @@
       <li>hh</li>
     </ul>
     <button type="button" class="btn btn-default">default</button>
+    <button type="button" class="btn btn-success">default</button>
+    <button type="button" class="btn btn-info">default</button>
+    <span class="input-group-btn">
+      <button type="button" class="btn btn-default">Go!</button>
+    </span>
+    <form action="" method="POST" class="form-horizontal" role="form">
+        <div class="form-group">
+          <legend>Form title</legend>
+        </div>
+    
+        
+    
+        <div class="form-group">
+          <div class="col-sm-10 col-sm-offset-2">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+    </form>
+    
     <SlideBar/>
     <FooterBar/>
   </div>
@@ -17,12 +36,12 @@
 <script>
 import HeaderBar from '~/components/HeaderBar.vue'
 import FooterBar from '~/components/FooterBar.vue'
+
 export default {
   components: {
     HeaderBar,
     FooterBar
   }
-
 }
 </script>
 
@@ -36,5 +55,4 @@ export default {
   font-size: 8em;
   color: burlywood;
 }
-
 </style>
